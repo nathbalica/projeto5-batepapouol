fd8b06cc8d7b9affd7d4284f2f71cc2fd7d857d7
nathbalica <nathbalica@gmail.com>
2023-04-16 17:37:53 -0300

    change get messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,79 +5,67 @@ let userName;
 let keepConnected;
 let logged = false;
 
-let inputLogin = document.querySelector('.input-name')
 let inputChat = document.querySelector('.input-write')
 
-function scrollToBottom() {
-  window.scrollTo(0, document.body.scrollHeight);
-}
-
-
-function renderChats() {
-  if (logged){
-    axios.get('https://mock-api.driven.com.br/api/vm/uol/messages')
-      .then(renderMessages)
-      .catch(erroMessage);
-  }
-}
-
 function renderMessages(response) {
-  const ulMessages = document.querySelector('.chats');
-  let messages = response.data;
-  let enteredUsers = [];
-  let exitedUsers = [];
-  let renderedMessages = [];
-
-  messages.forEach(message => {
-    let liClass = '';
-    let messageContent = '';
-
-    switch (message.type) {
-      case 'status':
-        if (message.text === 'entra na sala...') {
-          if (!enteredUsers.includes(message.from)) {
-            enteredUsers.push(message.from);
-          } else {
-            return;
+  if (logged){
+    const ulMessages = document.querySelector('.chats');
+    let messages = response.data;
+    let enteredUsers = [];
+    let exitedUsers = [];
+    let renderedMessages = [];
+  
+    messages.forEach(message => {
+      let liClass = '';
+      let messageContent = '';
+  
+      switch (message.type) {
+        case 'status':
+          if (message.text === 'entra na sala...') {
+            if (!enteredUsers.includes(message.from)) {
+              enteredUsers.push(message.from);
+            } else {
+              return;
+            }
+          } else if (message.text === 'sai da sala...') {
+            if (!exitedUsers.includes(message.from)) {
+              exitedUsers.push(message.from);
+            } else {
+              return;
+            }
           }
-        } else if (message.text === 'sai da sala...') {
-          if (!exitedUsers.includes(message.from)) {
-            exitedUsers.push(message.from);
-          } else {
-            return;
-          }
-        }
-        liClass = 'status-message enter-out-room';
-        messageContent = `<strong>${message.from}</strong> ${message.text}`;
-        break;
-      case 'message':
-        liClass = 'message public';
-        messageContent = `<strong>${message.from}</strong> para <strong>Todos:</strong> ${message.text}`;
-        break;
-      case 'private_message':
-        liClass = 'private-message private';
-        messageContent =  `<strong>${message.from}</strong> reservadamente para <strong>${message.to}</strong>: ${message.text}`;
-        break;
-      default:
-        console.error(`Unknown message type: ${message.type}`);
-        return;
-    }
-
-    const renderedMessage = `
-    <li data-test="message" class="${liClass}">
-    <span class="time">(${message.time})</span>
-    ${messageContent}</li>`;
-    
-    if (!renderedMessages.includes(renderedMessage)) {
-      renderedMessages.push(renderedMessage);
-    }
-  });
-
-  // Renderiza apenas as novas mensagens
-  const newMessages = renderedMessages.filter(message => !ulMessages.innerHTML.includes(message));
-  ulMessages.innerHTML += newMessages.join('');
-
-  scrollToBottom();
+          liClass = 'status-message enter-out-room';
+          messageContent = `<strong>${message.from}</strong> ${message.text}`;
+          break;
+        case 'message':
+          liClass = 'message public';
+          messageContent = `<strong>${message.from}</strong> para <strong>Todos:</strong> ${message.text}`;
+          break;
+        case 'private_message':
+          liClass = 'private-message private';
+          messageContent =  `<strong>${message.from}</strong> reservadamente para <strong>${message.to}</strong>: ${message.text}`;
+          break;
+        default:
+          console.error(`Unknown message type: ${message.type}`);
+          return;
+      }
+  
+      const renderedMessage = `
+      <li data-test="message" class="${liClass}">
+      <span class="time">(${message.time})</span>
+      ${messageContent}</li>`;
+      
+      if (!renderedMessages.includes(renderedMessage)) {
+        renderedMessages.push(renderedMessage);
+      }
+    });
+  
+    // Renderiza apenas as novas mensagens
+    const newMessages = renderedMessages.filter(message => !ulMessages.innerHTML.includes(message));
+    ulMessages.innerHTML += newMessages.join('');
+  
+    scrollToBottom();
+  }
 }
 
 function sendMessages(type='message'){
@@ -101,21 +89,11 @@ function sendMessages(type='message'){
     }
   }
 }
-  
 
-function userOnline(user) {
-  // Envia a requisição POST para manter o usuário online
-  const intervalId = setInterval(() => {
-    axios.post('https://mock-api.driven.com.br/api/vm/uol/status', user)
-      .catch(error => {
-          console.info("Nao foi possivel renderizar o chat!!!")
-      });
-  }, 5000);
-
-  // Para o envio da requisição quando o usuário sair da página
-  window.addEventListener('beforeunload', () => {
-    clearInterval(intervalId);
-  });
+function getMessages(){
+  axios.get("https://mock-api.driven.com.br/api/vm/uol/messages")
+  .then(renderMessages)
+  .catch(erroGetMessage)
 }
 
 function userRegister() {
@@ -136,13 +114,13 @@ function userRegister() {
   .then(response => {
     console.log(response);
     logged = true
-    renderChats();
+    getMessages();
     responseReceived(response);
   })
   .catch(existingdUser)
 
   
-  setInterval(renderChats, 3000);
+  setInterval(getMessages, 3000);
 
   keepConnected = setInterval(() => {
     axios.post('https://mock-api.driven.com.br/api/vm/uol/status', user)
@@ -151,9 +129,19 @@ function userRegister() {
 
 }
 
+function sucessGetMessage(response){
+  console.log(response);
+  sendMessages(response);
+}
+
 function responseReceived(response) {
   console.log(`sucesso ${response.data}!!!!! :D`);
   console.log(response);
+}
+
+function erroGetMessage(error){
+  console.log('Erro ao buscar as mensagens', error);
+  alert('Ocorreu um erro ao buscar as mensagens do chat! Tente novamente mais tarde!');
 }
 
 function erroKeepConnected(error){
@@ -185,5 +173,8 @@ inputChat.addEventListener('keypress', function(e){
   }
 }, false);
 
+function scrollToBottom() {
+  window.scrollTo(0, document.body.scrollHeight);
+}
 
 userRegister();
